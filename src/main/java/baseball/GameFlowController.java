86b58bc8f6da23c8989e0f7136e7c6c3b1b34cd8
Hwangseoeun <sunny030921@gmail.com>
2025-09-08 18:46:56 +0900

    feat: 숫자가 일치하는지 결과를 출력하는 기능 구현

--- src/main/java/baseball/GameFlowController.java
+++ src/main/java/baseball/GameFlowController.java
@@ -3,6 +3,11 @@ package baseball;
 import baseball.model.Numbers;
 import baseball.view.InputView;
 
+import java.util.Arrays;
+
+import static baseball.model.Numbers.isGuessedNumberMatchAnswer;
+import static baseball.view.OutputView.outputJudgeResult;
+
 public class GameFlowController {
 
     private final InputView inputView;
@@ -15,6 +20,17 @@ public class GameFlowController {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         final Numbers answerDigits = RandomNumberPicker.answerDigits();
-        final Numbers guessedNumberDigits = inputView.inputGuessedNumber();
+        System.out.println(Arrays.toString(answerDigits.getNumberDigits().toArray()));
+        boolean isGameContinue = true;
+
+        while (isGameContinue) {
+            final Numbers guessedNumberDigits = inputView.inputGuessedNumber();
+
+            isGameContinue = !isGuessedNumberMatchAnswer(answerDigits, guessedNumberDigits);
+
+            outputJudgeResult(answerDigits, guessedNumberDigits);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
