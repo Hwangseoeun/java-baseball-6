55702f18d4a0c5480339ffd95c1c7ce190133793
Hwangseoeun <sunny030921@gmail.com>
2025-09-08 18:47:22 +0900

    feat: 숫자가 일치하는지 결과를 출력하는 기능 구현

---
+++ src/main/java/baseball/model/ResultType.java
@@ -0,0 +1,18 @@
+package baseball.model;
+
+public enum ResultType {
+    BALL("볼"),
+    STRIKE("스트라이크"),
+    NOTHING("낫싱"),
+    ;
+
+    private final String message;
+
+    ResultType(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
