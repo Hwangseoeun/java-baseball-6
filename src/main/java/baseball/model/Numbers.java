0fb2597cbabc2f82f49b65f0f4473dc5d081aced
Hwangseoeun <sunny030921@gmail.com>
2025-09-10 10:20:26 +0900

    fix: final 추가

--- src/main/java/baseball/model/Numbers.java
+++ src/main/java/baseball/model/Numbers.java
@@ -6,11 +6,11 @@ import java.util.Set;
 
 public class Numbers {
 
-    private final static int ANSWER_SIZE = 3;
-    private final static int MIN_DIGIT = 1;
-    private final static int MAX_DIGIT = 9;
+    private static final int ANSWER_SIZE = 3;
+    private static final int MIN_DIGIT = 1;
+    private static final int MAX_DIGIT = 9;
 
-    List<Integer> numberDigits;
+    final List<Integer> numberDigits;
 
     public Numbers(final List<Integer> numberDigits) {
         validateNumberSize(numberDigits);
@@ -23,8 +23,8 @@ public class Numbers {
         return numberDigits;
     }
 
-    private static void validateNumberSize(List<Integer> splitGuessedNumber) {
-        Set<Integer> splitGuessedNumbers = new HashSet<>(splitGuessedNumber);
+    private void validateNumberSize(final List<Integer> splitGuessedNumber) {
+        final Set<Integer> splitGuessedNumbers = new HashSet<>(splitGuessedNumber);
 
         if(splitGuessedNumbers.size() != ANSWER_SIZE) {
             throw new IllegalArgumentException("잘못된 값입니다.");
@@ -32,17 +32,17 @@ public class Numbers {
     }
 
     //TODO: 현재 depth 2이기에 1로 줄이는 방법 생각 필요
-    private static void validateDigitRange(List<Integer> splitGuessedNumber) {
+    private void validateDigitRange(final List<Integer> splitGuessedNumber) {
         for(Integer number : splitGuessedNumber) {
-            if(number<MIN_DIGIT || number>MAX_DIGIT) {
+            if(number < MIN_DIGIT || number > MAX_DIGIT) {
                 throw new IllegalArgumentException("잘못된 값입니다.");
             }
         }
     }
 
-    public static boolean isGuessedNumberMatchAnswer(Numbers answerDigits, Numbers guessedNumberDigits) {
-        List<Integer> answer = answerDigits.getNumberDigits();
-        List<Integer> guessedNumber = guessedNumberDigits.getNumberDigits();
+    public static boolean isGuessedNumberMatchAnswer(final Numbers answerDigits, final Numbers guessedNumberDigits) {
+        final List<Integer> answer = answerDigits.getNumberDigits();
+        final List<Integer> guessedNumber = guessedNumberDigits.getNumberDigits();
 
         return answer.equals(guessedNumber);
     }
